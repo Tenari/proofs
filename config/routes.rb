2f004230eafb73056920ef7f9d450b9626148d02
Daniel Zapata <daniel@peoplepattern.com>
2017-05-10 20:00:47 -0500

    indicate how many objectiosn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :arguments
   resources :theorems
-  get '/theorems/:id/objections' => 'theorems#objections'
+  get '/theorems/:id/objections' => 'theorems#objections', as: :theorem_objections
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
