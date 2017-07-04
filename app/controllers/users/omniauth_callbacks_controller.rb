74e90f4ceb04fddaffc8540fe49d80a40e3c1b18
Daniel Zapata <daniel@peoplepattern.com>
2017-07-04 09:56:27 -0500

    handle handle nil

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,13 +6,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to (request.env['omniauth.origin'] || '/')
   end
   def google_oauth2
-    puts "you are here"
-    puts request.env["omniauth.auth"]
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    puts "user: #{@user.name}"
     sign_in @user, :event => :authentication #this will throw if @user is not activated
-    puts "signed in"
     redirect_to (request.env['omniauth.origin'] || '/')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,23 @@ class User < ActiveRecord::Base
   has_many :arguments
   has_many :theorems
   has_many :notifications
-  validates :handle, :uniqueness => true, format: {with: /\A[a-zA-Z0-9]+\z/ , message: "may only contain numbers and letters"}
+  #validates :handle, :uniqueness => true, format: {with: /\A[a-zA-Z0-9]+\z/ , message: "may only contain numbers and letters"}
 
   validate do
     self.errors.add(:handle, "cannot use that handle") if self.handle == 'theorems'
+    unless self.handle.nil?
+      other_user = User.where(handle: self.handle).first
+      self.errors.add(:handle, "handle already taken") if other_user
+
+      self.errors.add(:handle, "handle may only use a-z A-Z and 0-9") unless self.handle =~ /\A[a-zA-Z0-9]+\z/
+    end
   end
 
   def self.from_omniauth(auth)
-    puts auth.provider
-    puts auth.uid
     user = where(provider: auth.provider, uid: auth.uid).first_or_create
-    puts user.id
-    puts "first or created"
     user.name = auth.info.name
     user.email = auth.info.email
     user.save
-    puts user.id
 
     user
   end
